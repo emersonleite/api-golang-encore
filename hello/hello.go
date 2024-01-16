4d7f838c07105b52e9830309c4ed7edc0b197868
Emerson Leite <emerson.bl@gmail.com>
2024-01-16 16:54:29 -0300

    chore: remove endpoint

--- hello/hello.go
+++ hello/hello.go
@@ -17,12 +17,3 @@ func World(ctx context.Context, name string) (*Response, error) {
 type Response struct {
 	Message string
 }
-
-// World responds with a tailored message
-//
-//encore:api public path=/hello2/:name2
-func World2(ctx context.Context, name2 string) (*Response, error) {
-	msg := fmt.Sprintf("Hello, %s", name2)
-
-	return &Response{Message: msg}, nil
-}
